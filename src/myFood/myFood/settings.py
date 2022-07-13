17965624044dff4426e231c19bb464470cd891fd
Zander <86967831+Zander404@users.noreply.github.com>
2022-07-13 03:19:10 -0300

    Deploy da aplicação

--- src/myFood/myFood/settings.py
+++ src/myFood/myFood/settings.py
@@ -13,6 +13,10 @@ from cProfile import run
 
 import os
 from pathlib import Path
+import django
+import django_heroku
+
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
