7225431d1c7527018e5949f178ffe54a17c30c58
Zander <86967831+Zander404@users.noreply.github.com>
2022-07-13 02:49:39 -0300

    Deploy da aplicação

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn myFood.wsgi.application -b 0.0.0.0:$PORT
--- src/myFood/myFood/settings.py
+++ src/myFood/myFood/settings.py
@@ -131,6 +131,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'loja/static/media')
 
